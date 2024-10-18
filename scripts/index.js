af818de953b3c1b68cf15b25b3040033055a9c75
AJ <144151461+Ashley-Haltenhoff-Student@users.noreply.github.com>
2024-10-18 18:51:48 +0000

    Authorization code & playlist getting

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,21 @@
+async function getUserAuth() {
+    const clientId = "1ef017c3043342b1a5dc582848ac57cd";
+
+    // Request authorization from the user to access Spotify's resources on their behalf
+    const redirectURI = document.location.href;
+    const scope = 'user-read-private user-read-email';
+
+    const url = `https://accounts.spotify.com/authorize?` + new URLSearchParams({
+        response_type: 'code',
+        client_id: clientId,
+        scope: scope,
+        redirect_uri: redirectURI,
+    });
+
+    // Redirect to Spotify Authorization
+    window.location.href = url;
+}
+
 async function getToken() {
     // Codes that give access to the database
     const clientId = "1ef017c3043342b1a5dc582848ac57cd";
@@ -28,13 +46,12 @@ async function getToken() {
 
         // convert to json
         const result = await res.json();
+        console.log(result);
         return result;
     } catch (error) {
         console.error(error.message);
     }
 }
-
-var list = document.createElement('ul').appendChild(document.body);
 
 async function getTopTenSongs() {
     const token = getToken();
@@ -63,34 +80,49 @@ async function getTopTenSongs() {
     }
 }
 
-async function addToQueue(song, username) {
-    const url = "https://https://api.spotify.com/v1/" + username + "/player/queue";
-    
-    try {
-        const res = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Authorization': 'Bearer' + getToken()
-            }
-        });
-    } catch (error) {
-
-    }
-}
-
 
 async function getAllPlaylists(username) {
-    const url = 'https://api.spotify.com/v1/users/' + username + '/playlists';
+    var url = 'https://api.spotify.com/v1/users/' + username + '/playlists';
+    const token = await getToken();
 
     try {
         const res = await fetch (url, {
             headers: {
-                'Authorization': 'Bearer' + getToken()
+                Authorization: 'Bearer ' + token['access_token']
             }
         });
 
         var result = await res.json();
-        console.log(result);
+        console.log(result['items']);
+
+        const contentDiv = document.getElementById('content-div');
+        const list = document.createElement('ul');
+
+        for (playlist in result) {
+            const listItem = document.createElement('li');
+            listItem.value = playlist['name'];
+            list.appendChild(listItem);
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+async function pause() {
+    const url = "https://api.spotify.com/v1/me/player/pause?device_id=7762172D-EBB1-4AF6-B0C8-694A09F81769"
+    const token = await getToken();
+
+    try {
+        const res = await fetch (url, {
+            method: 'PUT',
+            headers: {
+                Authorization: 'Bearer ' + token['access_token']
+            }
+        });
+
+        var result = await res.json();
+        console.log(result['items']);
+
     } catch (error) {
         console.error(error.message);
     }
